75c1d6a343b93ec24209042789a93a4e57c073e7
Shane Duffy <shane@shaneduffy.io>
2024-01-16 23:53:38 -0800

    Add top nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,16 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { AudioRecorder } from './services/AudioRecorder/audio-recorder.service'
+import { NavTopComponent } from "./components/nav-top/nav-top.component";
 
 declare var window: any;
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [CommonModule, RouterOutlet, NavTopComponent]
 })
 export class AppComponent {
   constructor(private audioRecorder: AudioRecorder) { }
---
+++ src/app/components/nav-top/nav-top.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-nav-top',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './nav-top.component.html',
+  styleUrl: './nav-top.component.scss'
+})
+export class NavTopComponent {
+  constructor() { }
+}
